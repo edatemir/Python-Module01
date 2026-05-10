31d2540dfb489d6f3d15cb986f15eb13483f4eb3
root <root@Eda.localdomain>
2026-05-11 02:07:50 +0300

    ex2 yapıldı

--- 3.py
+++ 3.py
@@ -4,17 +4,27 @@ class Plant:
         self.height = height
         self.age = age
 
-    def grow(self, height):
+    def grow(self):
         self.height += 0.8
 
-    def age(self, age):
+    def age_up(self):
         self.age += 1
 
     def show(self):
         print(
             f"{self.name}: "
-            f"{self.height}cm, "
+            f"{round(self.height, 1)}cm, "
             f"{self.age} days old"
             )
 
-if 
+if __name__ == "__main__":
+    rose = Plant("Rose", 25.0, 30)
+    start_h = rose.height
+    print("=== Garden Plant Growth ===")
+    for day in range(1, 8):
+       print(f"=== Day {day} ===")
+       rose.grow()
+       rose.age_up()
+       rose.show()
+    growth = rose.height - start_h
+    print(f"Growth this week: {round(growth, 1)}cm")
